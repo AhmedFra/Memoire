7d10632aa5a9de5a133e28bf0efd3834bdfa1231
oussamazoy <121978258+oussamazoy@users.noreply.github.com>
2023-05-23 01:42:09 +0100

    profile pic edit

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,13 +1,12 @@
 import 'dart:io';
 
-
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
 import 'package:sheesh/models/user.dart';
 import 'package:sheesh/utilities/user_pref.dart';
 import 'package:sheesh/widgets/appbar_widget.dart';
 import 'package:sheesh/widgets/button_widget.dart';
-import 'package:sheesh/widgets/profile_widget.dart';
 import 'package:sheesh/widgets/textfield_widget.dart';
 
 class EditProfilePage extends StatefulWidget {
@@ -17,65 +16,86 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   User user = UserPreferences.myUser;
+  File? _image;
+
+  Future<void> _selectImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: source);
+
+    setState(() {
+      _image = pickedImage != null ? File(pickedImage.path) : null;
+    });
+  }
 
   @override
-  Widget build(BuildContext context) => 
-         Builder(
-          builder: (context) => Scaffold(
-            appBar: buildAppBar(context),
-            body: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              physics: BouncingScrollPhysics(),
-              children: [
-                ProfileWidget(
-                  imagePath: user.imagePath,
-                  isEdit: true,
-                  onClicked: () async {},
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        physics: BouncingScrollPhysics(),
+        children: [
+          GestureDetector(
+            onTap: () => _selectImage(ImageSource.gallery),
+            child: Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/cover3.jpg'),
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(height: 24),
-                TextFieldWidget(
-                  label: 'Full Name',
-                  text: user.name,
-                  onChanged: (name) {},
-                ),
-                const SizedBox(height: 24),
-                TextFieldWidget(
-                  label: 'Email',
-                  text: user.email,
-                  onChanged: (email) {},
-                ),
-                const SizedBox(height: 24),
-                TextFieldWidget(
-                  label: 'Description',
-                  text: user.description,
-                  maxLines: 5,
-                  onChanged: (about) {},
-                ),
-                const SizedBox(height: 24),
-                TextFieldWidget(
-                  label: 'Address',
-                  text: user.address,
-                  
-                  onChanged: (address) {},
-                ),
-                const SizedBox(height: 24),
-                TextFieldWidget(
-                  label: 'Phone number',
-                  text: user.phoneNumber,
-                 
-                  onChanged: (phoneNumber) {},
-                ),
-                const SizedBox(height: 24),
-                TextFieldWidget(
-                  label: 'Birthdate',
-                  text: user.birthdate,
+              ),
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.white,
+                size: 32,
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Full Name',
+            text: user.name,
+            onChanged: (name) {},
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Email',
+            text: user.email,
+            onChanged: (email) {},
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Description',
+            text: user.description,
+            maxLines: 5,
+            onChanged: (about) {},
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Address',
+            text: user.address,
+            onChanged: (address) {},
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Phone number',
+            text: user.phoneNumber,
+            onChanged: (phoneNumber) {},
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Birthdate',
+            text: user.birthdate,
                  
                   onChanged: (birthdate) {},
                 ),
-                const SizedBox(height: 50),
-              ],
-            ),
-          ),
-        )
-      ;
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
 }
+
